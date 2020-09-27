0e18a9b8e87b69a33a761ac0231d55e9af107cc0
Omid Parsa <omidparsa.1985@gmail.com>
2020-09-28 00:22:51 +0200

    added footer component

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,16 +3,25 @@ import React from 'react';
 
 import './footer.styles.scss';
 
+import {MdLocationCity, MdEmail} from 'react-icons/md';
+
 //COMPONENTS
 import HeadingTertiary from '../heading-tertiary/heading-tertiary.component';
 
 const Footer = () => (
     <div className="footer">
         <div className="row">
-            <div className="col-1-of-3">
-                <HeadingTertiary text='contact me' />
+            <HeadingTertiary text='contact us' />
+            <div>
+                <MdLocationCity className='footer__icon'/>
+                <span>Oslo, Norway</span>
+            </div>
+            <div>
+                <MdEmail className='footer__icon'/>
+                <span>omidparsa.1985@gmail.com</span>
             </div>
         </div>
     </div>
 ); 
+
 export default Footer;
